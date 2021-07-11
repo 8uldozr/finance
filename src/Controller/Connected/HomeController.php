e682055ca9bb3c3fc440c13da7e94925eca00b4f
florian <8uld0zr@pm.me>
2021-07-11 20:00:48 +0200

    v1

---
+++ src/Controller/Connected/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Connected;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="public_home")
+     */
+    public function home(): Response
+    {
+        return $this->render("public/home.html.twig");
+    }
+}
